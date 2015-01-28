2e2bdfbf00a7a4db52f443e51aa828b416b6f9cc
Denis Makhlin <denismakhlin@gmail.com>
2015-01-28 15:04:10 -0800

    1st Commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <!--Below is the code that sets up php code on my index.php page--> 
+        <?php
+//        I sent out an echo or a message that writes it out in text
+            echo "Web Server Is Running Locally";
+        ?>
+    </body>
+</html>
