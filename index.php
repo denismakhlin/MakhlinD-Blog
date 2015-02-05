82a99400f9a4485f8a86af1e2a16952f56e47826
Denis Makhlin <denismakhlin@gmail.com>
2015-02-05 15:03:03 -0800

    6th Commit

--- index.php
+++ index.php
@@ -1,19 +1,14 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
+
         <!--Below is the code that sets up php code on my index.php page--> 
         <?php
-        //Below is the line of code that links my file to my create-db.php page
+            //Below is the line of code that links/shows my file to my header 
+            //page
+            require_once (__DIR__ . "/view/header.php");
+            //Below is the line of code that shows my navigatioin page
+            require_once (__DIR__ . "/view/navigation.php");
+            //Below is the line of code that links my file to my create-db.php page
             require_once(__DIR__ . "/controller/create-db.php");
+            //Below is the line of code that links/shows my file to my footer 
+            //page
+            require_once (__DIR__ . "/view/footer.php");
         ?>
-    </body>
-</html>
